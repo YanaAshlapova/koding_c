5cc1cb9b75e08990e798dda79e6a9d2afd0847e0
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-02-17 13:59:18 +1000

    Update laba1.c

--- laba1.c
+++ laba1.c
@@ -16,35 +16,38 @@ int main()
     int x;
     
     do {
-    printf("Введите первый коэфицент ");
-    scanf("%f",&a);
+        printf("Введите первый коэфицент ");
+        scanf("%f",&a);
     
-    printf("Введите второй коэфицент ");
-    scanf("%f",&b);
+        printf("Введите второй коэфицент ");
+        scanf("%f",&b);
     
-    printf("Введите третий коэфицент ");
-    scanf("%f",&c);
-    
-
-    Discrim=b*b-4*a*c;
+        printf("Введите третий коэфицент ");
+        scanf("%f",&c);
     
     
+        Discrim=b*b-4*a*c;
+    
+    
+        if (Discrim>0)
+        {
+            x1=(-b+ sqrt(Discrim))/(2*a);
+            x2=(-b- sqrt(Discrim))/(2*a);
+            printf("x1=%f x2=%f\n", x1, x2);
+        } 
+        else if (Discrim==0)
+        {
+            x1=-b/(2*a);
+            printf("Один корень x1=%f\n", x1);
+        } 
+        else if (Discrim<0)
+        {
+            printf("Нет корней\n");
+        }
         
-    
-    if (Discrim>0){
-        x1=(-b+ sqrt(Discrim))/(2*a);
-        x2=(-b- sqrt(Discrim))/(2*a);
-        printf("x1=%f x2=%f\n", x1, x2);
-    } else if (Discrim==0){
-        x1=-b/(2*a);
-        printf("Один корень x1=%f\n", x1);
-    } else if (Discrim<0){
-        printf("Нет корней\n");
-    }
-    
-    printf("Нажмите 1 чтообы продолжить\n");
-    scanf("%d", &x);
+        printf("Нажмите 1 чтообы продолжить\n");
+        scanf("%d", &x);
     } while (x==1);
-return 0;
+    return 0;
     
 }
