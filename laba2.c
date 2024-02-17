a5da15af1b1df59894903687428370677771c91e
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-02-17 14:44:13 +1000

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -3,6 +3,8 @@
 int main() {
     double mas[3][3];
     double sum_main = 0.0;
+    double sum_pob = 0.0;
+    
     
     printf("Введите элементы массива 3x3:\n");
     for (int i = 0; i < 3; i++) {
@@ -14,8 +16,15 @@ int main() {
     for (int i = 0; i < 3; i++) {
         sum_main += mas[i] [i];
     }
+    
+    for (int i = 0; i < 3; i++) {
+        sum_pob += mas[i] [2-i];
+    }
+    
 
     printf("Сумма элементов главной диагонали:%lf\n",sum_main);
+    printf("Сумма элементов побочной диагонали:%lf\n",sum_pob);
+
     
     return 0;
 }
