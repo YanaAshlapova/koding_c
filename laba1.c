be253fa63f354e843108eee68c630cb11a5507db
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-02-17 15:06:10 +1000

    Update laba1.c

--- laba1.c
+++ laba1.c
@@ -1,53 +1,41 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-#include <math.h>
 
-int main()
-{
-    float a,b,c;
-    float Discrim, x1, x2;
+int main() {
+    int i,j ;
+    int N,M;
     int x;
     
     do {
-        printf("Введите первый коэфицент ");
-        scanf("%f",&a);
+        printf("Введите количество строк: ");
+        scanf("%d",&N);
+        printf("Введите количество столбцов: ");
+        scanf("%d",&M);
+        double mas[N][M];
+        double sum_main = 0.0;
+        double sum_pob = 0.0;
     
-        printf("Введите второй коэфицент ");
-        scanf("%f",&b);
+        
+        printf("Введите элементы массива:\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                scanf("%lf", &mas[i][j]);
+            }
+        }
     
-        printf("Введите третий коэфицент ");
-        scanf("%f",&c);
-    
-    
-        Discrim=b*b-4*a*c;
-    
-    
-        if (Discrim>0)
-        {
-            x1=(-b+ sqrt(Discrim))/(2*a);
-            x2=(-b- sqrt(Discrim))/(2*a);
-            printf("x1=%f x2=%f\n", x1, x2);
-        } 
-        else if (Discrim==0)
-        {
-            x1=-b/(2*a);
-            printf("Один корень x1=%f\n", x1);
-        } 
-        else if (Discrim<0)
-        {
-            printf("Нет корней\n");
+        for (int i = 0; i < N; i++) {
+            sum_main += mas[i] [i];
         }
         
+        for (int i = 0; i < N; i++) {
+            sum_pob += mas[i] [2-i];
+        }
+        
+    
+        printf("Сумма элементов главной диагонали:%lf\n",sum_main);
+        printf("Сумма элементов побочной диагонали:%lf\n",sum_pob);
+
         printf("Нажмите 1 чтообы продолжить\n");
         scanf("%d", &x);
     } while (x==1);
     return 0;
-    
 }
