03256e695d11414a1267a45ec7d2af9a5576e630
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-03-09 15:00:36 +1000

    Update laba21.c

--- laba21.c
+++ laba21.c
@@ -1,28 +1,37 @@
-#include <stdio.h> 
- 
-int main() { 
-    int arr[2][2]; 
-    int kv[2][2]; 
-     
-  
-    printf("Введите значения массива:\n"); 
-    for (int i = 0; i < 2; i++) { 
-        for (int j = 0; j < 2; j++) { 
-            scanf("%d", &arr[i][j]); 
-        } 
-    } 
-     
-     
-    for (int i = 0; i < 2; i++) { 
-        for (int j = 0; j < 2; j++) { 
-            kv[i][j] = arr[i][0] * arr[0][j] + arr[i][1] * arr[1][j]; 
+#include <stdio.h>
+
+int main() {
+    int i,j ;
+    int N,M;
+    int x;
+    
+    printf("Введите количество строк: ");
+    scanf("%d",&N);
+    printf("Введите количество столбцов: ");
+    scanf("%d",&M);
+    double mas[N][M];
+    int kv[N][M];
+
+    
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%lf", &mas[i][j]);
+        }
+    }
+
+     for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) { 
+            kv[i][j] = 0; 
+            for (int k = 0; k < M; k++) 
+                kv[i][j] += mas[i][k]*mas[k][j]; 
         } 
     } 
      
      
     printf("Квадрат матрицы:\n"); 
-    for (int i = 0; i < 2; i++) { 
-        for (int j = 0; j < 2; j++) { 
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) { 
             printf("%d ", kv[i][j]); 
         } 
         printf("\n"); 
