5f5559dad26e3b59ceaad554dfc5f4b932d71809
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-03-09 15:01:20 +1000

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -2,40 +2,35 @@
 
 int main() {
     int i,j ;
-    int N,M;
-    int x;
     
     printf("Введите количество строк: ");
-    scanf("%d",&N);
+    scanf("%d",&i);
     printf("Введите количество столбцов: ");
-    scanf("%d",&M);
-    double mas[N][M];
-    int kv[N][M];
+    scanf("%d",&j);
+    double mas[i][j];
+    double sum_main = 0.0;
+    double sum_pob = 0.0;
 
     
     printf("Введите элементы массива:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             scanf("%lf", &mas[i][j]);
         }
     }
 
-     for (int i = 0; i < N; i++) { 
-        for (int j = 0; j < M; j++) { 
-            kv[i][j] = 0; 
-            for (int k = 0; k < M; k++) 
-                kv[i][j] += mas[i][k]*mas[k][j]; 
-        } 
-    } 
-     
-     
-    printf("Квадрат матрицы:\n"); 
-    for (int i = 0; i < N; i++) { 
-        for (int j = 0; j < M; j++) { 
-            printf("%d ", kv[i][j]); 
-        } 
-        printf("\n"); 
-    } 
-     
-    return 0; 
+    for (int i = 0; i < 3; i++) {
+        sum_main += mas[i] [i];
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        sum_pob += mas[i] [2-i];
+    }
+    
+
+    printf("Сумма элементов главной диагонали:%lf\n",sum_main);
+    printf("Сумма элементов побочной диагонали:%lf\n",sum_pob);
+
+    
+    return 0;
 }
