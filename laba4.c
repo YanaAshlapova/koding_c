00ee36895c0fc3e096a71cd8ef290d1308b3e2e4
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-03-16 14:35:05 +1000

    Update laba4.c

--- laba4.c
+++ laba4.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
-#include <math.h>
+#include "tr.h"
 
-// Функция для вычисления периметра треугольника
-float calculatePerimeter(float side1, float side2, float side3) {
-    return side1 + side2 + side3;
-}
-
-// Функция для вычисления площади треугольника
-float calculateArea(float side1, float side2, float side3) {
-    float s = (side1 + side2 + side3) / 2;
-    return sqrt(s * (s - side1) * (s - side2) * (s - side3));
-}
-
-int main() {
-    float side1, side2, side3;
-
-    printf("Введите длины трех сторон треугольника: ");
-    scanf("%f %f %f", &side1, &side2, &side3);
-
-    // Проверка неравенства треугольника
-    if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-        float perimeter = calculatePerimeter(side1, side2, side3);
-        float area = calculateArea(side1, side2, side3);
-
-        printf("Периметр треугольника: %.2f\n", perimeter);
-        printf("Площадь треугольника: %.2f\n", area);
-    } else {
-        printf("Длины сторон не удовлетворяют условиям треугольника.\n");
-    }
-
+int main(){
+    int a,b,c,p;
+    double s;
+    printf("Введите a,b,c ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if side1<side2+side3 and side2<side1+side3 and side3<side1+side2{
+        p = perim(a,b,c);
+    printf("P=%d\n", p);
+    s = area(a,b,c);
+    printf("S=%lf", s);
     return 0;
+    }
+    
+}
+
+
+tr.h
+int perim(int side1, int side2, int side3);
+double area(int side1, int side2, int side3);
+
+
+tr.c
+#include <math.h>
+int perim(int side1, int side2, int side3){
+    int per;
+    per=side1+side2+side3;
+    return per;
+}
+
+double area(int side1, int side2, int side3){
+    double ar;
+    int per_2;
+    per_2= (side1+side2+side3)/2;
+    ar=sqrt(per_2*(per_2-side1)*(per_2-side2)*(per_2-side3));
+    return ar;
 }
