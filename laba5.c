dcd4b301441950a03a034b888c19ae9c2b64f6bf
YanaAshlapova <159601558+YanaAshlapova@users.noreply.github.com>
2024-03-31 12:48:09 +1000

    Update laba5.c

--- laba5.c
+++ laba5.c
@@ -1,4 +1,64 @@
-matrix_operations.c
+main.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix_operations.c"
+#include "matrix_result.c"
+
+
+
+int main() {
+    int n;
+    printf("Введите размерность матрицы n: ");
+    scanf("%d", &n);
+
+    double** matrix1 = (double**)malloc(n * sizeof(double*));
+    double** matrix2 = (double**)malloc(n * sizeof(double*));
+
+    for (int i = 0; i < n; i++) {
+        matrix1[i] = (double*)malloc(n * sizeof(double));
+        matrix2[i] = (double*)malloc(n * sizeof(double));
+    }
+
+    printf("Введите элементы первой матрицы:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &matrix1[i][j]);
+        }
+    }
+
+    printf("Введите элементы второй матрицы:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &matrix2[i][j]);
+        }
+    }
+
+    char operation;
+    printf("Введите операцию (+, -, *): ");
+    scanf(" %c", &operation);
+
+    double** result = matrix_operation(matrix1, matrix2, n, operation);
+
+    printf("Результат операции:\n");
+    matrix_result(result,n);
+
+    // Освобождаем выделенную память
+    for (int i = 0; i < n; i++) {
+        free(matrix1[i]);
+        free(matrix2[i]);
+        free(result[i]);
+    }
+    free(matrix1);
+    free(matrix2);
+    free(result);
+
+    return 0;
+}
+
+
+matrix_operatins.c
+    
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -37,62 +97,19 @@ double** matrix_operation(double** matrix1, double** matrix2, int n, char operat
     return result_matrix;
 }
 
-main.c
 
+matrix_result.c
+    
 #include <stdio.h>
-#include <stdlib.h>
-#include "matrix_operations.c"
 
-int main() {
-    int n;
-    printf("Введите размерность матрицы n: ");
-    scanf("%d", &n);
-
-    double** matrix1 = (double**)malloc(n * sizeof(double*));
-    double** matrix2 = (double**)malloc(n * sizeof(double*));
-
-    for (int i = 0; i < n; i++) {
-        matrix1[i] = (double*)malloc(n * sizeof(double));
-        matrix2[i] = (double*)malloc(n * sizeof(double));
-    }
-
-    printf("Введите элементы первой матрицы:\n");
-    for (int i = 0; i < n; i++) {
+void matrix_result(double** matrix, int n){
+    for (int i = 0; i< n; i++) {
         for (int j = 0; j < n; j++) {
-            scanf("%lf", &matrix1[i][j]);
-        }
-    }
-
-    printf("Введите элементы второй матрицы:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%lf", &matrix2[i][j]);
-        }
-    }
-
-    char operation;
-    printf("Введите операцию (+, -, *): ");
-    scanf(" %c", &operation);
-
-    double** result = matrix_operation(matrix1, matrix2, n, operation);
-
-    printf("Результат операции:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%lf ", result[i][j]);
+            printf("%lf ", matrix[i][j]);
         }
         printf("\n");
     }
-
-    // Освобождаем выделенную память
-    for (int i = 0; i < n; i++) {
-        free(matrix1[i]);
-        free(matrix2[i]);
-        free(result[i]);
-    }
-    free(matrix1);
-    free(matrix2);
-    free(result);
-
-    return 0;
 }
+
+
+matrix_create.c
